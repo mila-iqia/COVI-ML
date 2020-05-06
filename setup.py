044298a016419467e2b86b0bcf82ee5b396317f2
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2020-05-06 16:28:05 -0400

    Move tensorflow dependencies into his own extra

--- setup.py
+++ setup.py
@@ -13,5 +13,13 @@ setup(
     author='Nasim Rahaman',
     author_email='nasim.rahaman@tuebingen.mpg.de',
     description='Contact Tracing Transformer',
-    install_requires=requirements
+    install_requires=requirements,
+    extras_require={
+        'tensorflow': [
+            'tensorflow==2.1',
+            'tensorflow-addons==0.9.1',
+            'onnx==1.6',
+            'onnx-tf @ git+https://github.com/onnx/onnx-tensorflow.git@c4fc047e7ec71daa6aa8f71e9cc2ee9e5a3768b6#egg=onnx-tf'
+        ],
+    }
 )
